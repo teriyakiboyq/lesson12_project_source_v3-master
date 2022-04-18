52b32f53e4ce1685db5b02592ce72c348ac1713c
teriyakiboyq <glassyskyq@gmail.com>
2022-04-18 15:32:52 +0300

    Homework12

---
+++ loader/loader.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, request, render_template
+from functions import load_posts, uploads_posts
+import logging
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+loader_blueprint = Blueprint('loader', __name__, url_prefix='/post', static_folder='static',
+                             template_folder='templates')
+
+
+@loader_blueprint.route('/form/')
+def main():
+    return render_template("post_form.html")
+
+
+@loader_blueprint.route('/upload/', methods=["GET", "POST"])
+def upload():
+    try:
+        file = request.files['picture']
+        filename = file.filename
+        content = request.values['content']
+        posts = load_posts()
+        posts.append({
+            'pic': f'/uploads/images/{filename}',
+            'content': content
+        })
+        uploads_posts(posts)
+        file.save(f'uploads/{filename}')
+        if filename.split('.')[-1] not in ["png","jpeg","jpg"]:
+            logging.info('Некорректный тип файла')
+    except FileNotFoundError:
+        logging.error('Ошибка при загрузке файла')
+        return "<h1> Файл не найден </h1> "
+    else:
+        return render_template('post_uploaded.html', pic=f'/uploads/images/{filename}', content=content)
---
+++ main/main.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request, render_template
+import logging
+from functions import load_posts
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
+main_blueprint = Blueprint('main', __name__, template_folder='templates')
+
+
+@main_blueprint.route('/')
+def main():
+    return render_template("index.html")
+
+
+@main_blueprint.route('/search/')
+def search():
+    search_by = request.args['s']
+    logging.info(f'Слово для поиска: {search_by}')
+    posts = [x for x in load_posts() if search_by.lower() in x['content'].lower()]
+    return render_template("post_list.html", search_by=search_by, posts=posts)
